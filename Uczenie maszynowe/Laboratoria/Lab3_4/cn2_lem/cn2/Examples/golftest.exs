e4c0e96806ebc994b8b3d5550033cf67fd948bb7
LiLatee <marcin.hradowicz@gmail.com>
2020-10-02 11:49:06 +0200

    backup

---
+++ Uczenie maszynowe/Laboratoria/Lab3_4/cn2_lem/cn2/Examples/golftest.exs
@@ -0,0 +1,13 @@
+**EXAMPLE FILE**
+
+sunny 80 88 true Do_not_Play;
+overcast 83 78 false Play;
+rain 70 92 false Play;
+rain 78 80 false Play;
+overcast 65 70 true Do_not_Play;
+overcast 68 64 true Play;
+sunny 72 95 false Do_not_Play;
+sunny 75 70 true Play;
+overcast 72 90 true Play;
+sunny 81 75 false Play;
+rain 74 80 true Do_not_Play;
